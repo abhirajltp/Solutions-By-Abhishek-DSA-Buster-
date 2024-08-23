f54d5808a8e7a83d7f44d5d5992bca3eea94dd95
abhishek rajpoot <100178251+abhirajltp@users.noreply.github.com>
2024-08-23 21:18:45 +0530

    Update rverseArray.cpp

--- rverseArray.cpp
+++ rverseArray.cpp
@@ -1,1 +1,30 @@
+/* Time complexity  => O(n)
+   Space Complexity => O(1) 
+*/
+
+#include<iostream> 
+using namespace std;
+
+// function to reverse an Array in (In-Place) with constant extra space 
+
+void reverseArray( int arr[] , int size ){
+  if( size == 0)
+    return ;
+  int front = 0 , rear = size - 1;
+  while( front < rear ){
+    swap(arr[front] , arr[rear]);
+    front++ ; rear-- ;
+  }
+  
+}
+int main(){
+  int arr[8] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
+  reverseArray( arr , 8);
+  cout<<" The arry after reversal "<<endl;
+  for( int i = 0 ; i < 8 ; i++ ){
+    cout<<arr[i]<<" ";
+  } 
+  return 0;
+}
+
 
